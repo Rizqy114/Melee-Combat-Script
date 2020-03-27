d3e8b566e35bed75c39c6cb77062ec7e2276b749
Rizqy114 <59502719+Rizqy114@users.noreply.github.com>
2020-03-27 17:30:30 +0700

    Create PlayerCombat.cs

---
+++ PlayerCombat.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+
+    public Animator animator;
+
+    public Transform attackPoint;
+    public LayerMask enemyLayers;
+
+    public float attackRange = 0.5f;
+    public int attackDamage = 20;
+
+    public float attackRate = 2f;
+    float nextAttackTime = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time >= nextAttackTime)
+        {
+            if (Input.GetKey(KeyCode.E))
+           {
+                Attack();
+                nextAttackTime = Time.time + 1f / attackRate;
+           }
+        }
+    }
+
+    void Attack()
+    {
+        // Play an attack animation
+        animator.SetTrigger("Attack");
+
+        // Detect enemies in ange of attack
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        // Damage them
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<Enemy>().TakeDamage(20);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+            return;
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+}
